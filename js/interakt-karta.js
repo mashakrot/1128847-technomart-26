b32311a4b4ee6eece02528dab14888a98fca87f3
mashakrot <52287963+mashakrot@users.noreply.github.com>
2019-11-01 17:41:01 +0200

    Конец

--- js/interakt-karta.js
+++
@@ -1,9 +0,0 @@
-var letter = document.querySelector(".write-us");
-var popupLetter = document.querySelector(".letter");
-
-
-
-letter.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popupLetter.classList.add("letter-show");
-});
